f33edd97c696ce7001f48f75c4b460ff39d2af56
Benoit Legigan <ben.legigan@gmail.com>
2019-02-03 21:04:20 +0100

    Ajout du code de la calculatrice

---
+++ calculatrice.py
@@ -0,0 +1,40 @@
+# coding: utf8
+'''
+ _____       _            _       _        _          
+/  __ \     | |          | |     | |      (_)         
+| /  \/ __ _| | ___ _   _| | __ _| |_ _ __ _  ___ ___ 
+| |    / _` | |/ __| | | | |/ _` | __| '__| |/ __/ _ \
+| \__/\ (_| | | (__| |_| | | (_| | |_| |  | | (_|  __/
+ \____/\__,_|_|\___|\__,_|_|\__,_|\__|_|  |_|\___\___|
+'''
+
+'''
+Remplacez "pass" par le code nécessaire pour renvoyer l'addition de a par b
+'''
+def addition(a,b):
+    pass
+
+'''
+Remplacez "pass" par le code nécessaire pour renvoyer la soustraction de a par b
+'''
+def soustraction(a,b):
+    pass
+
+'''
+Remplacez "pass" par le code nécessaire pour renvoyer la multiplication de a par b
+'''
+def multiplication(a,b):
+    pass
+
+'''
+Remplacez "pass" par le code nécessaire pour renvoyer la division de a par b
+'''
+def division(a,b):
+    pass
+
+
+# MERCI DE NE PAS MODIFIER LE CODE EN DESSOUS
+print("22 + 47 = " + str(addition(22,47)))
+print("45 - 3 = " + str(soustraction(45,3)))
+print("6 * 8 = "+ str(multiplication(6,8)))
+print("150 / 10 = " + str(division(150,10)))
